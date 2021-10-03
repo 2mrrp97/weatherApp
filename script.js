2070cf9c62d62a14d88106244ed8c7d6457660da
Subhro Sen <88680402+2mrrp97@users.noreply.github.com>
2021-10-03 23:29:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var cityname = document.getElementById('cityname');
 
 submitBtn.addEventListener('click', () => {
 
-    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=" + cityname.value + "&lang=en&units=metric&mode=JSON", {
+    fetch("https://community-open-weather-map.p.rapidapi.com/forecast?q=kolkata&units=metric&mode=json&lang=en&cnt=6", {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
@@ -12,22 +12,58 @@ submitBtn.addEventListener('click', () => {
     })
         .then(response => response.json())
         .then(data => {
-            var info = document.getElementById('winfo');
-            var childs = info.children;
-
             console.log(data);
+            var info = document.getElementById('maindiv');
+            console.log(info);
 
-            childs[7].innerText = ""; // reset the default text inside the black box 
-            childs[0].innerText = "City : " + cityname.value;
-            childs[1].innerText = "Temp : " + data['main'].temp + " celcius";
-            childs[2].innerText = "Temp max : " + data['main'].temp_max + " celcius";
-            childs[3].innerText = "Temp min : " + data['main'].temp_min + " celcius";
-            childs[4].innerText = "Feels like : " + data['main'].feels_like + " celcius";
-            childs[5].innerText = "Weather : " + data['weather'][0]['main'];
-            childs[6].innerText = "Weather desc : " + data['weather'][0]['description'];
+            let index = 0;
+            populate_element(info, data, false, index++);
+
+            var forecast = document.getElementById('forecast_div');
+            forecast.innerHTML = '';
+
+            for (let i = 0; i < 5; i++) {
+                forecast.innerHTML += `<div class = "card py-5 px-3 col-lg-2 col-md-4 col-sm-8">
+                                            <h4 class="temp text-center my-2 mb-5"></h4>
+                                            <h6 class="temp text-center my-2"></h6>
+                                            <h6 class="temp text-center my-2"></h6>
+                                            <h6 class="temp text-center my-2"></h6>
+                                            <h6 class="temp text-center my-2"></h6>
+                                            <h6 class="temp text-center my-2"></h6>
+                                            <h6 class="temp text-center my-2"></h6>
+                                        </div>`;
+            }
+
+            var cards = forecast.children;
+            console.log(cards);
+            for (let i = 0; i < cards.length; i++, index++) {
+                populate_element(cards[i], data, true, index);
+            }
         })
         .catch(err => {
             winfo.innerHTML = '<div class = "text-center my-5 fw-bold py-4" style="font-size :2rem;">The city name you have given does not exist in our database ! OR something else went wrong please try again</div>';
             console.error(err);
         });
 });
+
+
+function populate_element(node, data, iscard, index) {
+    var childs = node.children;
+    if (7 < childs.length)
+        childs[7].innerText = ""; // reset the default text inside the black box 
+
+
+    if (iscard) {
+        var fheading = document.getElementById('fheading');
+        console.log(fheading);
+        fheading.style.display = "block";
+    }
+
+    childs[0].innerText = iscard ? "Date : " + data['list'][index].dt_txt.slice(0, 10) : "City : " + cityname.value;
+    childs[1].innerText = "Temp : " + data['list'][index]['main'].temp + " celcius";
+    childs[2].innerText = "Temp max : " + data['list'][index]['main'].temp_max + " celcius";
+    childs[3].innerText = "Temp min : " + data['list'][index]['main'].temp_min + " celcius";
+    childs[4].innerText = "Feels like : " + data['list'][index]['main'].feels_like + " celcius";
+    childs[5].innerText = "Weather : " + data['list'][index]['weather'][0]['main'];
+    childs[6].innerText = "Weather desc : " + data['list'][index]['weather'][0]['description'];
+};
